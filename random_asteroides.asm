81c2eb1b8e60e9ca34321ddecacf79508bc8d3ec
Jose Revete <joserevete02@gmail.com>
2024-07-12 20:58:08 -0400

    Avance

--- random_asteroides.asm
+++ random_asteroides.asm
@@ -11,6 +11,8 @@ letra: .space 2
 	.asciiz ""
 
 .text
+li $t4 2
+sw $t4 0xffff0000
 # Se cargan necesarias para el programa
 la $s0 reserva
 lw $t1 ast_izq
@@ -157,89 +159,22 @@ p_bucle_principal: subi $s0 $s0 540
 
 
 
-
-
 bucle_principal:
 	li $v0,32
 	li $a0,200
 	syscall
 	
 	li $t0 0
-	jal mover_piloto
 	jal mover_ast
-	jal reiniciar
+	j reiniciar
 	
-# Leer opcion del piloto (avanzar o retroceder
-mover_piloto:
-	addi $s0 $s0 459
-	li $v0 4
-	la $a0 pedir
-	syscall
-	
-	li $v0 8
-	la $a0 letra
-	li $a1 1
-	syscall
-	
-	li $v0 4
-	syscall
-	
-	beq $a0 87 avanzar
-	beq $a0 119 avanzar
-	beq $a0 83 retroceder
-	beq $a0 115 retroceder
-	j mover_piloto
-
-# Se avanza: se verifica si llego a la meta, si en la casilla 0 o 1 hay asteroide que colisione con la nave	
-avanzar:
-	beq $t5 3 volver
-	subi $s0 $s0 15
-	lb $t1 0($s0)
-	lb $t2 1($s0)
-	beq $t1 46 meta
-	or $t3 $t1 $t2
-	bnez $t3 verificar
-	
-	addi $s0 $s0 27
-	lb $t3 0($s0)
-	lb $t4 1($s0)
-	sb $zero 0($s0)
-	sb $zero 1($s0)
-	subi $s0 $s0 27
-	sb $t3 0($s0)
-	sb $t4 1($s0)
-	addi $s0 $s0 42
-	addi $t5 $t5 1
-	j avanzar
-
-# El jugador llego a la meta
-meta:
-	
-# Verificar si hay un asteroide > en la posicion 0 que al mever colisone con la posicion 1, y viceversa	
-verificar:
-	beq $t1 62 colision
-	beq $t2 60 colision
-	addi $s0 $s0 27
-	lb $t3 0($s0)
-	lb $t4 1($s0)
-	sb $zero 0($s0)
-	sb $zero 1($s0)
-	subi $s0 $s0 27
-	sb $t3 0($s0)
-	sb $t4 1($s0)
-	addi $s0 $s0 42
-	addi $t5 $t5 1
-	j avanzar
-	
-# Retroceder: Se debe crear una variable que indique la candtidad de veces que ha avanzado el piloto. Si la variable es igual a 0, entonces no puede retroceder
-retroceder:
 
 # Se mueven los asteroides de izquierda a derecha	
 mover_ast:
-	beq $t0 459 volver
+	beq $t0 458 reverse
 	lb $t1 1($s0)
 	beq $t1 60 izquierda
-	beq $t1 62 derecha
+#	beq $t1 62 derecha
 	addi $s0 $s0 1
 	addi $t0 $t0 1
 	j mover_ast
@@ -254,8 +189,8 @@ izquierda:
 
 # Caso 1: .<     ., debemos tener .    <.	
 ir_al_final:
-	li $t2 32
-	sb $t2 1($s0)
+
+	sb $zero 1($s0)
 	addi $s0 $s0 25
 	sb $t1 1($s0)
 	addi $s0 $s0 2
@@ -264,44 +199,61 @@ ir_al_final:
 	
 # Caso 2: .   <   ., debemos tener .   <     .
 mover_izquierda:
-	li $t2 32
-	sb $t2 1($s0)
+
+	sb $zero 1($s0)
 	sb $t1 0($s0)
 	addi $s0 $s0 1
 	addi $t0 $t0 1
 	j mover_ast
 	
+	
+reverse: 
+	beqz $t0 volver
+	lb $t1 0($s0)
+	beq $t1 62 derecha
+	subi $s0 $s0 1
+	subi $t0 $t0 1
+	j reverse
+	
 # Si el asteroide es >, entoneces dos casos
 derecha:
-	lb $t2 2($s0)
+	lb $t2 1($s0)
 	beq $t2 46 ir_al_comienzo
 	beqz $t2 mover_derecha
 	j colision
 
 # Caso 1: .    >., entonces .>     .
 ir_al_comienzo:
-	li $t2 32
-	sb $t2 1($s0)
+
+	sb $zero 0($s0)
 	subi $s0 $s0 25
-	sb $t1 1($s0)
-	addi $s0 $s0 2
-	addi $t0 $t0 26
-	j mover_ast
+	sb $t1 0($s0)
+	subi $s0 $s0 26
+	subi $t0 $t0 26
+	j reverse
 
 # Caso 2: .   >   ., entonces .     >  .
 mover_derecha:
-	li $t2 32
-	sb $t2 1($s0)
-	sb $t1 2($s0)
-	addi $s0 $s0 1
-	addi $t0 $t0 1
-	j mover_ast
+
+	sb $zero 0($s0)
+	sb $t1 1($s0)
+	subi $s0 $s0 1
+	subi $t0 $t0 1
+	j reverse
 	
 # Se reinician las variables y se deja en 0,0 el tablero
 reiniciar:
+	subi $s0 $s0 27
+	li $t0 0
+	li $t1 0
+	jal imprimir
+	subi $s0 $s0 540
+	j bucle_principal
+	
 
 # Ayuda para los jal y jr
 volver:
+	
 	jr $ra
 	
 # Ocurrio una colision	
@@ -311,7 +263,223 @@ colision:
 	syscall
 	j end
 
+imprimir:
+	beq $t1 21 volver
+	beq $t0 27 sgte
+	li $v0 11
+	lb $t3 0($s0)
+	
+	beqz $t3 espacio
+
+	move $a0 $t3
+	syscall
+	
+	addi $s0 $s0 1
+	addi $t0 $t0 1
+	j imprimir
+
+# Si hay un 0, se imprime un espacio	
+espacio:
+	addi $t3 $t3 32
+	move $a0 $t3
+	syscall
+	
+	addi $s0 $s0 1
+	addi $t0 $t0 1
+	j print
+
+sgte:
+	li $v0 4
+	la $a0 nl
+	syscall
+	
+	li $t0 0
+	addi $t1 $t1 1
+	j print
+
+
 # Fin
 end:	
 	li $v0 10
 	syscall
+
+
+
+
+
+###################################################################
+###                                                             ###
+###################################################################
+
+
+
+
+
+.kdata
+_choque: .asciiz "Ocurrio colision"
+_no_se_puede: .asciiz "No se puede retroceder del home"
+
+.ktext 0x80000180
+
+	lw $k0 0xffff0004
+	
+# Leer opcion del piloto (avanzar o retroceder
+mover_piloto:
+	jal buscar
+	beq $k0 87 avanzar
+	beq $k0 119 avanzar
+	beq $k0 83 retroceder
+	beq $k0 115 retroceder
+	j mover_piloto
+
+buscar:
+	lb $t8 0($s0)
+	beq $t8 47 regresar
+	addi $s0 $s0 1
+	addi $t7 $t7 1
+	j buscar
+	
+regresar: jr $ra
+
+# Se avanza: se verifica si llego a la meta, si en la casilla 0 o 1 hay asteroide que colisione con la nave	
+avanzar:
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	beq $t1 46 meta
+	or $t3 $t1 $t2
+	bnez $t3 verificar
+
+	lb $t3 0($s0)
+	lb $t4 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	subi $s0 $s0 27
+	sb $t3 0($s0)
+	sb $t4 1($s0)
+	addi $s0 $s0 54
+	
+	lb $t3 0($s0)
+	lb $t4 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	subi $s0 $s0 27
+	sb $t3 0($s0)
+	sb $t4 1($s0)
+	addi $s0 $s0 54
+	
+	lb $t3 0($s0)
+	lb $t4 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	subi $s0 $s0 27
+	sb $t3 0($s0)
+	sb $t4 1($s0)
+	sub $s0 $s0 $t7
+	addi $s0 $s0 27
+	
+	j fin_interrupcion
+
+
+# El jugador llego a la meta
+meta:
+	addi $s0 $s0 27
+	li $t3 32
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $t3 0($s0)
+	sb $t3 1($s0)
+	addi $s0 $s0 459
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	
+	subi $s0 $s0 432
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $t3 0($s0)
+	sb $t3 1($s0)
+	addi $s0 $s0 432
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	
+	subi $s0 $s0 405
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $t3 0($s0)
+	sb $t3 1($s0)
+	addi $s0 $s0 405
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	j fin_interrupcion
+	
+	
+# Verificar si hay un asteroide > en la posicion 0 que al mever colisone con la posicion 1, y viceversa	
+verificar:
+	beq $t1 62 colision_k
+	beq $t2 60 colision_k
+	addi $s0 $s0 27
+	lb $t3 0($s0)
+	lb $t4 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	subi $s0 $s0 27
+	sb $t3 0($s0)
+	sb $t4 1($s0)
+	addi $s0 $s0 42
+	addi $t5 $t5 1
+	j avanzar
+	
+# Retroceder: Se debe crear una variable que indique la candtidad de veces que ha avanzado el piloto. Si la variable es igual a 0, entonces no puede retroceder
+retroceder:
+	blez $t9 no
+	addi $s0 $s0 81
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	or $t3 $t1 $t2
+	bnez $t3 verificar
+	
+	subi $s0 $s0 27
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	addi $s0 $s0 27
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	
+	subi $s0 $s0 54
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	addi $s0 $s0 27
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	
+	subi $s0 $s0 54
+	lb $t1 0($s0)
+	lb $t2 1($s0)
+	sb $zero 0($s0)
+	sb $zero 1($s0)
+	addi $s0 $s0 27
+	sb $t1 0($s0)
+	sb $t2 1($s0)
+	sub $s0 $s0 $t7
+	subi $s0 $s0 27
+	
+	j fin_interrupcion
+
+no:
+	li $v0 4
+	la $a0 _no_se_puede
+	syscall
+	j fin_interrupcion
+	
+# Ocurrio una colision	
+colision_k:
+	li $v0 4
+	la $a0 _choque
+	syscall
+	
+fin_interrupcion:
+	li $t7 0
+	eret
